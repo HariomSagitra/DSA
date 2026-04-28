2e44cb06e9326aeb61d44a10f718674956f877b3
Hariom Sagitra <hariomdhakad750910@gmail.com>
2026-04-28 18:45:16 +0530

    Array Basics Questions

---
+++ 27april/arry.js
@@ -0,0 +1,34 @@
+// let array = new Array(5);
+
+// for (let i = 0; i < array.length; i++) {
+//   array[i] = Number(prompt("please Enter number"));
+// }
+// console.log(array);
+
+// let arr = [10,30,40,67]
+// let sum = 0;
+
+// for(let i=0;i<arr.length;i++){
+//     sum = sum + arr[i]
+// }
+// console.log(sum)
+
+// let arr = [24,33,85,66,77]
+// let max = arr[0]
+
+// for(let i=0;i<arr.length;i++){
+//     if(max<arr[i]){
+//         max = arr[i]
+//     }
+// }
+// console.log(max);
+
+// let arr = [232, 33, 55, 667, 88];
+// let min = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (min > arr[i]) {
+//     min = arr[i];
+//   }
+// }
+// console.log(min);
